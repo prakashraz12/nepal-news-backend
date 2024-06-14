1c73486e6e5c73f5e050c6aa4bf25f06d2101312
Prakash Shrestha <rzprakash16@gmail.com>
2024-06-14 07:59:30 +0545

    fix/multer-file-path

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,13 +1,15 @@
 import multer from "multer";
 
-//temp config
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "./public/temp"); // Uploads will be stored in the public/temp
+      cb(null, "../public/temp")
     },
     filename: function (req, file, cb) {
-        cb(null,file.originalname);
-    },
-});
-
-export const upload = multer({ storage });
+      
+      cb(null, file.originalname)
+    }
+  })
+  
+export const upload = multer({ 
+    storage, 
+})
